1b35b0bbc006a08ab3d1ef47e1583dfa127c6db7
Sandeep Kumar Shah <63727946+sandeep0009@users.noreply.github.com>
2024-01-20 20:55:20 +0530

    Update cloud.js

--- backend/cloud.js
+++ backend/cloud.js
@@ -3,8 +3,8 @@
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: 'dcksy2dka', 
-  api_key: '764137741616223', 
-  api_secret: 'Uzuct6S1222BVfQrARZ2QGRhuXE' 
+  cloud_name: '', 
+  api_key: '', 
+  api_secret: '' 
 });
 export default cloudinary
